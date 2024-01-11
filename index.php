e5738931d0c3e278e27f7bd631ddddb9f92bc122
Shanthosh Kumar <shanthoshkumar2003@gmail.com>
2024-01-11 20:03:51 +0530

    Added Dean segment

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <!-- <link rel="stylesheet" href="index.css"> -->
     <?php require 'src\components\Header\header.php' ?>
+    <?php require 'src\components\Dean\dean.php' ?> 
     <?php require 'src\components\VisionMission\vision_mission.php' ?> 
     <?php require 'src\components\Categories\categoriesCards.php' ?>
     <?php require 'src\components\Footer\footer.php' ?>
