1962f36ca1a58249d796e798f5d2d029d653f784
Praveen Prabhu <praveenzues@gmail.com>
2024-01-11 14:38:06 +0530

    Added header

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html>
-    <?php require 'src\components\Categories\categoriesCards.php' ?> 
+    <!-- <link rel="stylesheet" href="index.css"> -->
+    <?php require 'src\components\Header\header.php' ?>
+    <?php require 'src\components\Categories\categoriesCards.php' ?>
     <?php require 'src\components\Footer\footer.php' ?>
 </html>
---
+++ src/components/Header/header.php
@@ -0,0 +1,107 @@
+<head>
+    <link rel="stylesheet" href="src\components\Header\header.css">
+</head>
+<header class="header-container noselect">
+	<a class="active" href="/" aria-current="page">
+		<h1 class="HeaderLogo">MIT - AU</h1>
+	</a>
+	<div class="burger-menu"><span class="sus "></span><span class="sus "></span><span class="sus "></span></div>
+	<nav>
+		<ul class="menus nohighlights noselect ">
+			<li class="menu-items">
+				<button type="button" aria-haspopup="menu" aria-expanded="false">
+					<a class="active" href="/" aria-current="page">Home</a>
+					<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+						<path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+					</svg>
+				</button>
+				<ul class="dropdown  ">
+					<li class="menu-items"><a class="" href="https://www.annauniv.edu/">AU Home</a></li>
+				</ul>
+			</li>
+			<li class="menu-items">
+				<button type="button" aria-haspopup="menu" aria-expanded="false">
+					<a class="" href="/departments/">Departments</a>
+					<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+						<path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+					</svg>
+				</button>
+				<ul class="dropdown  ">
+					<li class="menu-items"><a class="" href="/departments/undefined"></a></li>
+					<li class="menu-items">
+						<button type="button" aria-haspopup="menu" aria-expanded="false">
+							<a class="" href="/departments/aero/">Aeronautical Engineering</a>
+							<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+								<path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+							</svg>
+						</button>
+						<ul class="dropdown dropdown-submenu ">
+							<li class="menu-items"><a class="" href="/departments/aero/undefined"></a></li>
+							<li class="menu-items">
+								<button type="button" aria-haspopup="menu" aria-expanded="false">
+									<a class="" href="/departments/aero/research/">Research</a>
+									<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+										<path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+									</svg>
+								</button>
+								<ul class="dropdown dropdown-submenu ">
+									<li class="menu-items"><a class="" href="/departments/aero/research/undefined"></a></li>
+									<li class="menu-items"><a class="" href="/departments/aero/research/publications">Publications</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li class="menu-items"><a class="" href="https://mitindia.edu/en/auto-home">Automobile Engineering</a></li>
+					<li class="menu-items"><a class="" href="https://mitindia.edu/en/ece-home">Electronics Engineering</a></li>
+					<li class="menu-items"><a class="" href="https://mitindia.edu/en/instrumentation-home">Instrumentation Engineering</a></li>
+					<li class="menu-items"><a class="" href="https://mitindia.edu/en/pt-home">Production Technology</a></li>
+					<li class="menu-items"><a class="" href="https://mitindia.edu/en/rpt-home">Rubber and Plastics Technology</a></li>
+					<li class="menu-items"><a class="" href="http://www.it.mitindia.edu/">Information Technology</a></li>
+					<li class="menu-items"><a class="" href="http://www.ct.mitindia.edu/dct/#/home">Computer Technology</a></li>
+					<li class="menu-items"><a class="" href="http://www.dash.mitindia.edu/">Applied Science and Humanities</a></li>
+				</ul>
+			</li>
+			<li class="menu-items">
+				<button type="button" aria-haspopup="menu" aria-expanded="false">
+					<a class="" href="/administration/">Administration</a>
+					<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+						<path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+					</svg>
+				</button>
+				<ul class="dropdown  ">
+					<li class="menu-items"><a class="" href="/administration/undefined"></a></li>
+					<li class="menu-items"><a class="" href="https://annauniv.irins.org/profile/43400">Dean</a></li>
+					<li class="menu-items"><a class="" href="/administration/officeStaff/">Office Staff</a></li>
+				</ul>
+			</li>
+			<li class="menu-items">
+				<button type="button" aria-haspopup="menu" aria-expanded="false">
+					<a class="" href="/centres">Centres</a>
+					<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+						<path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+					</svg>
+				</button>
+				<ul class="dropdown  ">
+					<li class="menu-items"><a class="" href="https://cc.mitindia.edu/">Computer Centre</a></li>
+					<li class="menu-items"><a class="" href="https://www.au-kbc.org/">AU-KBC Research Centre</a></li>
+					<li class="menu-items"><a class="" href="http://www.casr.mitindia.edu/">Centre for Aerospace Research</a></li>
+				</ul>
+			</li>
+			<li class="menu-items">
+				<button type="button" aria-haspopup="menu" aria-expanded="false">
+					<a class="" href="/facilities">Facilities</a>
+					<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+						<path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
+					</svg>
+				</button>
+				<ul class="dropdown  ">
+					<li class="menu-items"><a class="" href="http://www.placement.mitindia.edu/">Placements</a></li>
+				</ul>
+			</li>
+			<li class="menu-items"><a class="" href="/about">About Us</a></li>
+			<li class="menu-items"><a class="" href="/scholarships">Scholarships</a></li>
+			<li class="menu-items"><a class="" href="/Staff/:id">Test</a></li>
+		</ul>
+	</nav>
+	<div class="login-container"><button class="login-button">Sign In</button></div>
+</header>
