923832eb52a41d974d9a966d34ae05a733b212f9
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-01-12 21:49:09 +0530

    Modified paths

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="./src/components/Footer/footer.css">
-<link rel="stylesheet" href="./src/components/Header/header.css">
+<link rel="stylesheet" href="index.css">
 </head>
 <html>
-    <?php require './src/components/Header/header.php'; ?>
-    <?php require './src/components/Centres/centre.php'; ?> 
-    <?php require './src/components/Dean/dean.php'; ?> 
-    <?php require './src/components/VisionMission/vision_mission.php'; ?> 
-    <?php require './src/components/Facilities/categoryCards.php'; ?>
-    <?php require './src/components/Footer/footer.php'; ?>
+    <?php include './src/components/Header/header.php'; ?>
+    <?php include './src/components/Centres/centre.php'; ?> 
+    <?php include './src/components/Dean/dean.php'; ?> 
+    <?php include './src/components/VisionMission/vision_mission.php'; ?> 
+    <?php include './src/components/Facilities/categoryCards.php'; ?>
+    <?php include './src/components/Footer/footer.php'; ?>
 </html>
