780f26b1a0edf65d009fc45872ff0f482d2052fc
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-01-13 19:11:32 +0530

    Changed css path

--- src/components/Facilities/categoryCards.php
+++ src/components/Facilities/categoryCards.php
@@ -1,5 +1,5 @@
 <head>
-    <link rel="stylesheet" href="src\components\Facilities\categoryCards.css">
+    <link rel="stylesheet" href="src/components/Facilities/categoryCards.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 </head>
 <?php
