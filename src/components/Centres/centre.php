c64dc99483a6459cdc6169754a56e16aa793e256
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-01-13 15:30:18 +0530

    Added centres

--- src/components/Centres/centre.php
+++ src/components/Centres/centre.php
@@ -1,12 +1,75 @@
 <head>
     <link rel="stylesheet" href="src/components/Centres/centre.css">
 </head>
-<div class="staggered-cards-container noselect">
-    <div class="description">
-        <h1>Centres</h1>
-    </div>
-    <div class="staggered-cards-wrapper" id="centre-wrapper">
-        <!-- Cards will be inserted here by JavaScript -->
-    </div>
-</div>
-<script src="src\components\Centres\centre.js"></script>
+<?php
+$centersData = [
+    [
+        "title" => "Computer Center",
+        "img" => "path_to_ComputerCenterImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "AUKBC - MIT",
+        "img" => "path_to_AUKBCImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "Center for Aerospace",
+        "img" => "path_to_CentreForAerospaceImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "Center for IoT",
+        "img" => "path_to_CenterForIoTImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "Library",
+        "img" => "path_to_LibraryImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "Placement",
+        "img" => "path_to_PlacementImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "Sports",
+        "img" => "path_to_SportsImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "Health Center",
+        "img" => "path_to_HealthCentreImage",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "SCoE",
+        "img" => "path_to_Scoe",
+        // "description" => "2K+ Active Members",
+    ],
+    [
+        "title" => "CEAT",
+        "img" => "path_to_CeatImage",
+        // "description" => "2K+ Active Members",
+    ],
+];
+
+echo '<div class="staggered-cards-container noselect">';
+echo '<div class="description">';
+echo '<h1>Centres</h1>';
+echo '</div>';
+echo '<div class="staggered-cards-wrapper">';
+
+foreach ($centersData as $index => $data) {
+    echo '<div class="card">';
+    echo '<img src="' . $data['img'] . '" />';
+    echo '<div class="content">';
+    echo '<h1>' . $data['title'] . '</h1>';
+    echo '</div>';
+    echo '</div>';
+}
+
+echo '</div>';
+echo '</div>';
+?>
