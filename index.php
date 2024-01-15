27f24047b3f63400a7e4170353c822ca955f3ac6
Praveen Prabhu <praveenzues@gmail.com>
2024-01-15 19:10:05 +0530

    Updated file structure and added new CSS styles

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <link rel="stylesheet" href="index.css">    
+    <link rel="stylesheet" href="src/index.css">   
+    <link rel="stylesheet" href="src/App.css">   
     <div id="root">
-        <?php include './src/components/Header/header.php'; ?>
-        <?php include './src/components/Banner/banner.php'; ?>
-        <?php include './src/components/NumberCount/numbercount1.php'; ?>
-        <?php include './src/components/HorizontalCards/horizontalcards.php'; ?>
-        <?php include './src/components/NumberCount/numbercount2.php'; ?>
-        <?php include './src/components/Centres/centre.php'; ?> 
-        <?php include './src/components/Dean/dean.php'; ?> 
-        <?php include './src/components/VisionMission/vision_mission.php'; ?> 
-        <?php include './src/components/Facilities/categoryCards.php'; ?>
-        <?php include './src/components/Footer/footer.php'; ?>
+        <section class="app-container">
+            <?php include './src/components/Header/header.php'; ?>
+            <section class="page-container">
+                <?php include './src/components/Banner/banner.php'; ?>
+                <?php include './src/components/NumberCount/numbercount1.php'; ?>
+                <?php include './src/components/HorizontalCards/horizontalcards.php'; ?>
+                <?php include './src/components/NumberCount/numbercount2.php'; ?>
+                <?php include './src/components/Centres/centre.php'; ?> 
+                <?php include './src/components/Dean/dean.php'; ?> 
+                <?php include './src/components/VisionMission/vision_mission.php'; ?> 
+                <?php include './src/components/Facilities/categoryCards.php'; ?>
+                <?php include './src/components/Footer/footer.php'; ?>
+            </section>
+        </section>
     </div>
 </html>
