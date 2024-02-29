8a2521c5698bfc3735d9931dce1c30b95009fe9d
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-02-29 21:14:40 +0530

    Reorder components

--- index.php
+++ index.php
@@ -26,11 +26,11 @@
             <section class="page-container">
                 <?php include './src/components/Banner/banner.php'; ?>
                 <?php include './src/components/NumberCount/numbercount1.php'; ?>
-                <?php include './src/components/HorizontalCards/horizontalcards.php'; ?>
-                <?php include './src/components/NumberCount/numbercount2.php'; ?>
-                <?php include './src/components/Centres/centre.php'; ?> 
                 <?php include './src/components/Dean/dean.php'; ?> 
+                <?php include './src/components/NumberCount/numbercount2.php'; ?>
                 <?php include './src/components/VisionMission/vision_mission.php'; ?> 
+                <?php include './src/components/HorizontalCards/horizontalcards.php'; ?>
+                <?php include './src/components/Centres/centre.php'; ?> 
                 <?php include './src/components/Facilities/categoryCards.php'; ?>
                 <?php include './src/components/Footer/footer.php'; ?>
             </section>
