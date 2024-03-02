aa60f16b15e279491e71bed954bcb36e8645fe14
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-03-02 23:23:29 +0530

    Refactor footer

--- src/components/Footer/footer.php
+++ src/components/Footer/footer.php
@@ -17,7 +17,7 @@ $footerContents = [
                 "url" => "",
             ],
             [
-                "title" => "Centre for Research",
+                "title" => "CSRC",
                 "url" => "http://ctdt.annauniv.edu/",
             ],
             [
@@ -27,21 +27,9 @@ $footerContents = [
         ],
     ],
     [
-        "title" => "Administration",
+        "title" => "Anna University",
         "url" => "#",
         "content" => [
-            [
-                "title" => "Dean",
-                "url" => "https://annauniv.irins.org/profile/43400",
-            ],
-            [
-                "title" => "People",
-                "url" => "https://mitindia.edu/en/administration/office-page",
-            ],
-            [
-                "title" => "Departments",
-                "url" => "https://mitindia.edu/en/all-departments",
-            ],
             [
                 "title" => "Acts & Statues",
                 "url" => "https://www.annauniv.edu/pdf/Acts%20&%20Statues-New.pdf",
@@ -49,6 +37,18 @@ $footerContents = [
             [
                 "title" => "Mandatory Disclosure",
                 "url" => "https://iqac.annauniv.edu/",
+            ],
+            [
+                "title" => "Genuineness Verification",
+                "url" => "https://gverify.annauniv.edu/",
+            ],
+            [
+                "title" => "NIRF",
+                "url" => "https://mitindia.edu/en/administration/office-page",
+            ],
+            [
+                "title" => "University Departments",
+                "url" => "https://www.annauniv.edu/univdept.php",
             ],
         ],
     ],
@@ -61,8 +61,12 @@ $footerContents = [
                 "url" => "https://docs.google.com/forms/d/e/1FAIpQLSdfAkiupN8aSNQynN383_njOlsF-E_UWQY50S0DHOGLSzcxBw/viewform",
             ],
             [
-                "title" => "Network Issues",
-                "url" => "https://docs.google.com/forms/d/e/1FAIpQLSdccEYSatG7fShAYwLMJ0hwvfvtDtW_cc00ZpGliYpfeYzhBA/viewform",
+                "title" => "MS Teams ID Requisition Form",
+                "url" => "https://www.annauniv.edu/rcc/pdf/form%20creating%20MS%20teams.pdf",
+            ],
+            [
+                "title" => "Internet Complaint Booking",
+                "url" => "http://bit.ly/mitcccbf",
             ],
             [
                 "title" => "More",
@@ -93,47 +97,33 @@ $footerContents = [
         ],
     ],
 ];
-
-$latitude = 12.948199763058907;
-$longitude = 80.13975384299005;
-
-$googleUrl = "https://www.google.com/maps/place/Madras+Institute+of+Technology,+Anna+University/@12.9474333,80.1403601,16.73z/data=!4m7!3m6!1s0x3a525fac595c29ff:0xb76082ae18b51418!4b1!8m2!3d12.948048!4d80.139742!16zL20vMDFjczgx?entry=ttu";
-
-echo '<div class="footer-container noselect">';
-echo '<span>';
-echo '<span>';
-echo '<h1>Anna University, MIT Campus</h1>';
-echo '<p>Chromepet, Chennai - 600044.</p>';
-echo '<p>dean@mitindia.edu | +044 2251 6002</p>';
-echo '<div class="map-contact-container">';
-echo '<div class="map-container">';
-echo '<div class="map-wrapper" style="height: 110%; width: 110%;">';
-echo '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.371661499109!2d80.13716707454623!3d12.948053215395197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525fac595c29ff%3A0xb76082ae18b51418!2sMadras%20Institute%20of%20Technology%2C%20Anna%20University!5e0!3m2!1sen!2sin!4v1704879128210!5m2!1sen!2sin" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo '<div class="copyright">';
-echo '© 2023 All Rights Reserved By <a href="#!"> @MIT WebTeam </a>';
-echo '</div>';
-echo '</span>';
-echo '</span>';
-echo '<span>';
-
-foreach ($footerContents as $index => $item) {
-    echo '<span>';
-    echo '<div class="column-title">' . $item['title'] . '</div>';
-    echo '<span>';
-
-    if (isset($item['content'])) {
-        foreach ($item['content'] as $subItem) {
-            echo '<a href="' . $subItem['url'] . '" target="_blank">' . $subItem['title'] . '</a>';
-        }
-    }
-
-    echo '</span>';
-    echo '</span>';
-}
-
-echo '</span>';
-echo '</div>';
 ?>
+
+<div class="footer">
+    <div class="footer-wrapper">
+        <div class="footer-row">
+            <?php foreach ($footerContents as $section) : ?>
+                <div class="footer-column">
+                    <div class="footer-title"><?php echo $section['title']; ?></div>
+                    <?php foreach ($section['content'] as $link) : ?>
+                        <a class="footer-link" target="_blank" rel="noreferrer" href="<?php echo $link['url']; ?>"><?php echo $link['title']; ?></a>
+                    <?php endforeach; ?>
+                </div>
+            <?php endforeach; ?>
+            <div class="footer-center">
+                <div>
+                    <i class="fa-solid fa-location-dot"> </i>
+                    <p><span>Anna University, MIT Campus</span> Chrompet, Chennai 600044</p>
+                </div>
+                <div>
+                    <i class="fa fa-phone"></i>
+                    <p>044 2251 6002</p>
+                </div>
+                <div>
+                    <i class="fa fa-envelope"></i>
+                    <p>dean@mitindia.edu</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
