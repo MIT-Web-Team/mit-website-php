b3abcec1436983ca333fb877dcf12f41825c2bac
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-03-02 13:55:38 +0530

    Refactor dropdown submenu

--- src/components/Header/header.php
+++ src/components/Header/header.php
@@ -15,14 +15,16 @@
 			<ul class="menus nohighlights noselect ">
 				<li class="menu-items">
 					<button type="button" aria-haspopup="menu" aria-expanded="false">
-						<a class="active" aria-current="page">Home</a>
+						<a class="active" aria-current="page" class="home">Home</a>
 						<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
 							<path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path>
 						</svg>
 					</button>
 					<ul class="dropdown  ">
-						<li class="menu-items"><a class="" href="/about">About MIT</a></li>
+						<li class="menu-items"><a class="" href="/">MIT Home</a></li>
 						<li class="menu-items"><a class="" href="https://www.annauniv.edu/">AU Home</a></li>
+						<li class="menu-items"><a class="" href="/about">About MIT</a></li>
+						<li class="menu-items"><a class="" href="/https://www.annauniv.edu/organogram.pdf">Organogram</a></li>
 					</ul>
 				</li>
 				<li class="menu-items">
@@ -33,39 +35,15 @@
 						</svg>
 					</button>
 					<ul class="dropdown">
-						<li class="menu-items"><a class="" href="/departments/undefined"></a></li>
-						<!-- <li class="menu-items">
-							<button type="button" aria-haspopup="menu" aria-expanded="false">
-								<a class="" href="/departments/aero/">Aeronautical Engineering</a>
-								<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-									<path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
-								</svg>
-							</button>
-							<ul class="dropdown dropdown-submenu ">
-								<li class="menu-items"><a class="" href="/departments/aero/undefined"></a></li>
-								<li class="menu-items">
-									<button type="button" aria-haspopup="menu" aria-expanded="false">
-										<a class="" href="/departments/aero/research/">Research</a>
-										<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-											<path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
-										</svg>
-									</button>
-									<ul class="dropdown dropdown-submenu ">
-										<li class="menu-items"><a class="" href="/departments/aero/research/undefined"></a></li>
-										<li class="menu-items"><a class="" href="/departments/aero/research/publications">Publications</a></li>
-									</ul>
-								</li>
-							</ul>
-						</li> -->
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/aero-home">Aeronautical Engineering</a></li>
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/auto-home">Automobile Engineering</a></li>
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/ece-home">Electronics Engineering</a></li>
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/instrumentation-home">Instrumentation Engineering</a></li>
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/pt-home">Production Technology</a></li>
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/rpt-home">Rubber and Plastics Technology</a></li>
-						<li class="menu-items"><a class="" href="http://www.it.mitindia.edu/">Information Technology</a></li>
-						<li class="menu-items"><a class="" href="http://www.ct.mitindia.edu/dct/#/home">Computer Technology</a></li>
-						<li class="menu-items"><a class="" href="http://www.dash.mitindia.edu/">Applied Sciences and Humanities</a></li>
+						<li class="menu-items"><a target="_blank" href="https://mitindia.edu/en/aero-home">Aeronautical Engineering</a></li>
+						<li class="menu-items"><a target="_blank" href="https://mitindia.edu/en/auto-home">Automobile Engineering</a></li>
+						<li class="menu-items"><a target="_blank" href="https://mitindia.edu/en/ece-home">Electronics Engineering</a></li>
+						<li class="menu-items"><a target="_blank" href="https://mitindia.edu/en/instrumentation-home">Instrumentation Engineering</a></li>
+						<li class="menu-items"><a target="_blank" href="https://mitindia.edu/en/pt-home">Production Technology</a></li>
+						<li class="menu-items"><a target="_blank" href="https://mitindia.edu/en/rpt-home">Rubber and Plastics Technology</a></li>
+						<li class="menu-items"><a target="_blank" href="http://www.it.mitindia.edu/">Information Technology</a></li>
+						<li class="menu-items"><a target="_blank" href="http://www.ct.mitindia.edu/dct/#/home">Computer Technology</a></li>
+						<li class="menu-items"><a target="_blank" href="http://www.dash.mitindia.edu/">Applied Sciences and Humanities</a></li>
 					</ul>
 				</li>
 				<li class="menu-items">
@@ -76,9 +54,13 @@
 						</svg>
 					</button>
 					<ul class="dropdown  ">
-						<li class="menu-items"><a class="" href="/administration/undefined"></a></li>
+						<li class="menu-items"><a class="" href="https://www.annauniv.edu/#vcmsg">VC's Desk</a></li>
+						<li class="menu-items"><a class="" href="https://www.annauniv.edu/administration.php">Registrar</a></li>
 						<li class="menu-items"><a class="" href="https://annauniv.irins.org/profile/43400">Dean</a></li>
-						<li class="menu-items"><a class="" href="/administration/officestaff">Office Staff</a></li>
+						<li class="menu-items"><a class="" href="https://annauniv.irins.org/profile/43400">Head of the Departments</a></li>
+						<li class="menu-items"><a class="" href="https://annauniv.irins.org/profile/43400">Centre's Directors</a></li>
+						<li class="menu-items"><a class="" href="/administration/officestaff">Dean Office Staff</a></li>
+						<li class="menu-items"><a class="" href="/administration/officestaff">Maintenance Office Staff</a></li>
 					</ul>
 				</li>
 				<li class="menu-items">
@@ -89,12 +71,37 @@
 						</svg>
 					</button>
 					<ul class="dropdown  ">
-						<li class="menu-items"><a class="" href="https://www.au-kbc.org/">AU-KBC Research Centre</a></li>
-						<li class="menu-items"><a class="" href="http://www.casr.mitindia.edu/">Centre for Aerospace Research</a></li>
-						<li class="menu-items"><a class="" href="http://www.ciot.mitindia.edu/">Centre for IoT</a></li>
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/research-centres/mit_scoe">SCoE</a></li>
-						<li class="menu-items"><a class="" href="https://mitindia.edu/en/research-centres/ceat_mit">CEAT</a></li>
-						<li class="menu-items"><a class="" href="https://cra.mitindia.edu/">CRA</a></li>
+						<li class="menu-items">
+							<button type="button" aria-haspopup="menu" aria-expanded="false">
+								<a class="">University</a>
+								<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+									<path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+								</svg>
+							</button>
+							<ul class="dropdown dropdown-submenu ">
+								<li class="menu-items"><a class="" href="https://cfr.annauniv.edu/">Centre for Research</a></li>
+								<li class="menu-items"><a class="" href="https://cac.annauniv.edu/">Centre for Academic Courses</a></li>
+								<li class="menu-items"><a class="" href="https://ctdt.annauniv.edu/">Centre for Sponsored Research and Consultancy</a></li>
+								<li class="menu-items"><a class="" href="https://acoe.annauniv.edu/">Additional Controller for Examinations</a></li>
+								<li class="menu-items"><a class="" href="https://cfd.annauniv.edu/">Centre for Faculty Development</a></li>
+							</ul>
+						</li>
+						<li class="menu-items">
+							<button type="button" aria-haspopup="menu" aria-expanded="false">
+								<a class="">MIT Campus</a>
+								<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+									<path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+								</svg>
+							</button>
+							<ul class="dropdown dropdown-submenu ">
+								<li class="menu-items"><a class="" href="https://www.au-kbc.org/">AU-KBC Research Centre</a></li>
+								<li class="menu-items"><a class="" href="http://www.casr.mitindia.edu/">Centre for Aerospace Research</a></li>
+								<li class="menu-items"><a class="" href="http://www.ciot.mitindia.edu/">Centre for Internet of Things</a></li>
+								<li class="menu-items"><a class="" href="https://mitindia.edu/en/research-centres/mit_scoe">Siemens Centre of Excellence</a></li>
+								<li class="menu-items"><a class="" href="https://mitindia.edu/en/research-centres/ceat_mit">Centre for Excellence in Automobile Technology</a></li>
+								<li class="menu-items"><a class="" href="https://cra.mitindia.edu/">Centre for Robotics and Automation </a></li>
+							</ul>
+						</li>
 					</ul>
 				</li>
 				<li class="menu-items">
@@ -112,15 +119,14 @@
 						<li class="menu-items"><a class="" href="http://www.health-centre.mitindia.edu/health_centre/">Health Centre</a></li>
 						<li class="menu-items"><a class="" href="https://library.annauniv.edu/mit_index.php">Library</a></li>
 						<li class="menu-items"><a class="" href="https://www.hostel.mitindia.edu/">Hostel</a></li>
+						<li class="menu-items"><a class="" href="https://mitindia.edu/en/alumni-home">MITAA</a></li>
 					</ul>
 				</li>
-				<!-- <li class="menu-items"><a class="" href="/about">About Us</a></li> -->
 				<li class="menu-items"><a class="" href="/scholarships">Scholarships</a></li>
-				<!-- <li class="menu-items"><a class="" href="/Staff/:id">Test</a></li> -->
 				<li class="menu-items"><a class="" href="/events">Events</a></li>
 			</ul>
 		</nav>
-		<?php 
+		<?php
 		if (isset($_SESSION["admin"]) && $_SESSION["admin"]) { ?>
 			<div class="login-container"><button class="login-button" onclick="signOut()">Sign Out</button></div>
 		<?php } else { ?>
@@ -137,7 +143,7 @@
 				</form>
 			</div>
 		</div>
-	</div>	
+	</div>
 </header>
 <script>
 	function showModal() {
@@ -146,32 +152,61 @@
 	}
 
 	document.addEventListener('DOMContentLoaded', function() {
+		var isMobile = window.matchMedia("only screen and (max-width: 768px)").matches;
 		var menuItems = document.querySelectorAll('.menu-items');
 
 		menuItems.forEach(function(menuItem) {
 			var button = menuItem.querySelector('button');
 			var dropdown = menuItem.querySelector('.dropdown');
+			var dropdownSubmenu = menuItem.querySelector('.dropdown dropdown-submenu');
 
-			if (button && dropdown) {
-				button.addEventListener('click', function() {
-					var expanded = button.getAttribute('aria-expanded') === 'true' || false;
-					if (expanded) {
-						// Close the dropdown
+			if (isMobile) {
+				if (button && dropdown) {
+					button.addEventListener('click', function() {
+						var expanded = button.getAttribute('aria-expanded') === 'true' || false;
+						if (expanded) {
+							dropdown.classList.remove('show');
+							button.setAttribute('aria-expanded', 'false');
+						} else {
+							dropdown.classList.add('show');
+							button.setAttribute('aria-expanded', 'true');
+						}
+					});
+
+					menuItem.addEventListener('mouseleave', function() {
 						dropdown.classList.remove('show');
 						button.setAttribute('aria-expanded', 'false');
-					} else {
-						// Close all other dropdowns
-						closeAllDropdowns();
-						// Open the clicked dropdown
+					});
+				}
+				if (button && dropdown && dropdownSubmenu) {
+					button.addEventListener('click', function() {
+						var expanded = button.getAttribute('aria-expanded') === 'true' || false;
+						if (expanded) {
+							dropdownSubmenu.classList.remove('show');
+							button.setAttribute('aria-expanded', 'false');
+						} else {
+							dropdownSubmenu.classList.add('show');
+							button.setAttribute('aria-expanded', 'true');
+						}
+					});
+
+					menuItem.addEventListener('mouseleave', function() {
+						dropdown.classList.remove('show');
+						button.setAttribute('aria-expanded', 'false');
+					});
+				}
+			} else {
+				if (button && dropdown) {
+					button.addEventListener('mouseenter', function() {
 						dropdown.classList.add('show');
 						button.setAttribute('aria-expanded', 'true');
-					}
-				});
+					});
 
-				menuItem.addEventListener('mouseleave', function() {
-					dropdown.classList.remove('show');
-					button.setAttribute('aria-expanded', 'false');
-				});
+					menuItem.addEventListener('mouseleave', function() {
+						dropdown.classList.remove('show');
+						button.setAttribute('aria-expanded', 'false');
+					});
+				}
 			}
 		});
 
@@ -203,14 +238,14 @@
 	};
 
 	function signOut() {
-    // Send an AJAX request to the server
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/src/apiservices/logout.php", true);
-    xhr.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            window.location.href = "/";
-        }
-    };
-    xhr.send();
-}
+		// Send an AJAX request to the server
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "/src/apiservices/logout.php", true);
+		xhr.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				window.location.href = "/";
+			}
+		};
+		xhr.send();
+	}
 </script>
