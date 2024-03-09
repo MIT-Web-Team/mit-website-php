9f68fedaeb59d3cf208baf5b78bae4737828a404
Praveen Prabhu <praveenzues@gmail.com>
2024-03-09 18:15:57 +0530

    Update css paths

--- src/pages/public/directors.php
+++ src/pages/public/directors.php
@@ -4,12 +4,12 @@
     <link rel="icon" href="src/assets/mit_logo.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Centre Directors</title>
-    <link rel="stylesheet" href="../../index.css">   
-    <link rel="stylesheet" href="../../App.css"> 
-    <link rel="stylesheet" href="../../pages/public/directors.css">
-    <link rel="stylesheet" href="../../components/Header/header.css">
-    <link rel="stylesheet" href="../../components/Footer/footer.css">
-    <link rel="stylesheet" href="../../components/Modal/modal.css">
+    <link rel="stylesheet" href="/src/index.css">   
+    <link rel="stylesheet" href="/src/App.css"> 
+    <link rel="stylesheet" href="/src/pages/public/directors.css">
+    <link rel="stylesheet" href="/src/components/Header/header.css">
+    <link rel="stylesheet" href="/src/components/Footer/footer.css">
+    <link rel="stylesheet" href="/src/components/Modal/modal.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
